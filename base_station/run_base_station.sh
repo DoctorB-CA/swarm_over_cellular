d761465e08723c1ce47fb96139f550e053d47d6a
omermad73 <madmon.omer@gmail.com>
2025-07-14 21:36:25 +0300

    fixed giu launcher

--- base_station/run_base_station.sh
+++ base_station/run_base_station.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# Activate venv
+# 1) Activate venv
 source "$(dirname "$0")/../venv/bin/activate"
 
-# Clean slate for Qt
+# 1a) Point DISPLAY at the Windows host (default gateway)
+export DISPLAY=$(ip route | awk '/^default/ {print $3}'):0.0
+export LIBGL_ALWAYS_INDIRECT=1
+
+# 2) Clean slate for Qt
 unset QT_PLUGIN_PATH
 unset LD_LIBRARY_PATH
 
-# Only PyQt5 platform plugins
+# 3) Only PyQt5 platform plugins
 export QT_QPA_PLATFORM_PLUGIN_PATH="$VIRTUAL_ENV/lib/python3.10/site-packages/PyQt5/Qt5/plugins/platforms"
 export QT_QPA_PLATFORM=xcb
 
-# Launch
+# 4) Launch
 python3 launch_gui.py
